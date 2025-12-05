ea2af3dc15a33205a98bb0582e5caffa0fa2f0c0
netseraph <netseraph@gmail.com>
2025-12-05 08:24:21 +0800

    debug browser.switch_to.window

--- main.py
+++ main.py
@@ -60,16 +60,18 @@ def auto_elearning(browser):
             _ele_lesson_title.click()
             # 焦点切换到课程标签页
             _handles = browser.window_handles
-            _message = f"当前窗口的句柄:{browser.current_window_handle},所有窗口的句柄:{_handles}"
-            debuginfo(0, _message)
+            debuginfo(
+                0,
+                f"当前窗口的句柄:{browser.current_window_handle},所有窗口的句柄:{_handles}",
+            )
             browser.switch_to.window(_handles[1])
 
             # 定位视频列表
             ele_list_videolist = browser.find_elements(
                 by=By.CLASS_NAME, value="list.el-row.is-align-middle.el-row--flex"
             )
-            _message = f"本课程共有{len(ele_list_videolist)}个视频"
-            debuginfo(1, _message)
+
+            debuginfo(1, f"本课程共有{len(ele_list_videolist)}个视频")
 
             for ele_video in ele_list_videolist:
                 _ele_video_title = ele_video.find_element(
@@ -82,8 +84,8 @@ def auto_elearning(browser):
                 _duration = 0
                 for item in split:
                     _duration = _duration * 60 + int(item)
-                _message = f"视频: [{_ele_video_title.text}] 时长: {_duration}秒"
-                debuginfo(2, _message)
+
+                debuginfo(2, f"视频: [{_ele_video_title.text}] 时长: {_duration}秒")
 
                 # 如果找到已完成标记,说明视频播放完整
                 if is_element_exist(
@@ -115,16 +117,18 @@ def auto_elearning(browser):
                     debuginfo(3, "视频加载完毕开始播放.")
                     _ele_play.click()
                     # 强制等待,直到视频播放完毕
-                    _message = f"延时{_duration+5}秒,以便完整播放视频."
-                    debuginfo(3, _message)
+
+                    debuginfo(3, f"延时{_duration+5}秒,以便完整播放视频.")
 
                     sleep(_duration + 5)
 
-                    _message = (
-                        f"当前窗口的句柄:{browser.current_window_handle},"
-                        "所有窗口的句柄:{_handles_video}"
+                    debuginfo(
+                        3,
+                        (
+                            f"当前窗口的句柄:{browser.current_window_handle},"
+                            f"所有窗口的句柄:{_handles_video}"
+                        ),
                     )
-                    debuginfo(3, _message)
 
                     # 关闭视频标签页
                     debuginfo(3, "关闭视频标签页")
@@ -132,6 +136,7 @@ def auto_elearning(browser):
 
                     # 焦点切换回课程标签页
                     browser.switch_to.window(_handles[1])
+                    debuginfo(3, browser.current_window_handle)
 
                     # browser.refresh()
                     # 关闭课程标签页
@@ -163,10 +168,10 @@ def auto_elearning_simple(browser):
         _ele_lesson_progress = _ele_lesson.find_element(
             by=By.CLASS_NAME, value="el-progress__text"
         )
-        _message = (
-            f"课程: [{_ele_lesson_title.text}] 已完成: {_ele_lesson_progress.text}"
+
+        debuginfo(
+            0, (f"课程: [{_ele_lesson_title.text}] 已完成: {_ele_lesson_progress.text}")
         )
-        debuginfo(0, _message)
 
         if _ele_lesson_progress.text != "100%":
             # 点击课程标题,在新的标签页打开未完成的课程.
@@ -203,8 +208,9 @@ def auto_elearning_simple(browser):
             )
             _learnd = _ele_learnd.text
 
-            _message = f"本课程总时长为{_chapterduration}分钟,已累计学习{_learnd}分钟."
-            debuginfo(1, _message)
+            debuginfo(
+                1, f"本课程总时长为{_chapterduration}分钟,已累计学习{_learnd}分钟."
+            )
 
             # 定位开始学习按钮
             _ele_start_btn = browser.find_element(
@@ -235,15 +241,21 @@ def auto_elearning_simple(browser):
             _duration = (
                 int(_chapterduration) - int(_learnd)
             ) * 61  # 每分钟按61秒计算,适当增加冗余
-            _message = f"延时{_duration}秒({datetime.timedelta(seconds=_duration)}),以便完整播放视频."
-            debuginfo(2, _message)
+
+            debuginfo(
+                2,
+                f"延时{_duration}秒({datetime.timedelta(seconds=_duration)}),以便完整播放视频.",
+            )
+
             sleep(_duration)
 
-            _message = (
-                f"当前窗口的句柄:{browser.current_window_handle},"
-                "所有窗口的句柄:{_handles_video}"
+            debuginfo(
+                3,
+                (
+                    f"当前窗口的句柄:{browser.current_window_handle},"
+                    "所有窗口的句柄:{_handles_video}"
+                ),
             )
-            debuginfo(3, _message)
 
             # 关闭视频标签页
             debuginfo(3, "关闭视频标签页")
@@ -251,11 +263,25 @@ def auto_elearning_simple(browser):
 
             # 焦点切换回课程标签页
             browser.switch_to.window(_handles[1])
+            debuginfo(
+                3,
+                (
+                    f"当前窗口的句柄:{browser.current_window_handle},"
+                    "所有窗口的句柄:{_handles_video}"
+                ),
+            )
             # 关闭课程标签页
             browser.close()
 
             # 焦点切换回培训班标签页
             browser.switch_to.window(_handles[0])
+            debuginfo(
+                3,
+                (
+                    f"当前窗口的句柄:{browser.current_window_handle},"
+                    "所有窗口的句柄:{_handles_video}"
+                ),
+            )
             browser.refresh()
 
 
